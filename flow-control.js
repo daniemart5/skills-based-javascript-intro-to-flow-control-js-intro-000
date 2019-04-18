88042856edfbea2912f2a9844a8213a6cc2fdb9c
Danielle Martinoli <daniechris5@gmail.com>
2019-04-18 23:53:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,9 @@
 function basicTeenager(age) {
   if age>=== 13 && <=== 19
-    return true
+    return "You are a teenager!"
+    else {
+      return "You are not a teenager"
+    }
 }
 
 function teenager(age) {
